52e31af9c5cd841997908ff926d1014a6e85bdf9
Nicolas Joseph <nicolas@nicolasjoseph.com>
2022-09-04 15:11:21 -0700

    feat: add authentication to the app with a fauna backend

---
+++ apps/banana-bot/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,36 @@
+import NextAuth from "next-auth"
+import DiscordProvider from "next-auth/providers/discord"
+
+import { Client as FaunaClient } from "faunadb"
+import { FaunaAdapter } from "@next-auth/fauna-adapter"
+
+const client = new FaunaClient({
+  secret: process.env.FAUNA_SECRET || "",
+  scheme: "https",
+  domain: "db.us.fauna.com"
+})
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_ID || "",
+      clientSecret: process.env.DISCORD_SECRET || "",
+    }),
+    // ...add more providers here
+  ],
+  adapter: FaunaAdapter(client),
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      const isAllowedToSignIn = !!user.id && (profile.email?.endsWith("nicolasjoseph.com") || false)
+      if (isAllowedToSignIn) {
+        return true
+      } else {
+        // Return false to display a default error message
+        return false
+        // Or you can return a URL to redirect to:
+        // return '/unauthorized'
+      }
+    }
+  }
+})
